a0fcda1c9ac58be28785af1a73f97e68d3446353
SantiagoS3 <43120219+SantiagoS3@users.noreply.github.com>
2020-04-13 21:00:27 -0500

    Add files via upload

---
+++ laboratorioocho/src/laboratorioocho/Nodo.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorioocho;
+
+/**
+ *
+ * @author hp
+ */
+public class Nodo {
+    private int dato;
+    //auto referencia
+    private Nodo enlace;
+    /**
+     * constructor nodo
+     * Se inicializan los atributos de la case
+     * por defecto
+     */
+    public Nodo(){
+        this.dato = 0;
+        this.enlace = null;
+    }
+    /**
+     * constructor Nodo
+     * inicializa el atributo dato de la clase.
+     * @param dato 
+     */
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.enlace = null;
+    }
+    
+    public int getDato() {
+        return dato;
+    }
+    /**
+     * Se actualiza el dato del nodo
+     * @param dato 
+     */
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+    /**
+     * obtiene el valor de la referencia
+     * @return referencia del siguiente nodo
+     */
+    public Nodo getEnlace() {
+        return enlace;
+    }
+    /**
+     * Se actualiza la referencia del nodo
+     * @param enlace 
+     */
+    public void setEnlace(Nodo enlace) {
+        this.enlace = enlace;
+    }    
+}
